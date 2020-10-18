a55a80fbf796b5494ba10f80d92939d5abd2aade
Mike Gorczynski <gorczynski.mike@gmail.com>
2020-10-18 19:48:44 +0200

    Added one .java file.

---
+++ src/main/java/com/mgorczyn/gradletest/GradleTest.java
@@ -0,0 +1,7 @@
+package com.mgorczyn.gradletest;
+
+public class GradleTest {
+    public static void main(String[] args) {
+        System.out.println("A test runner for a gradle project.");
+    }
+}
